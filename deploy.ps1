162b75730eaae6b66b28289f7ac82c0a84201744
Johannes Pandis <johannes.pandis@dataformers.at>
2019-02-21 17:33:33 +0100

    deploy script also copies .npmrc

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,5 @@
 $srcPath = $PSScriptRoot + "/build/*" 
 $destPath = $PSScriptRoot + "/deploy"
 Copy-Item "$srcPath" "$destPath" -Recurse
-Copy-Item "$PSScriptRoot\package.json" "$PSScriptRoot\deploy\package.json" -Force 
+Copy-Item "$PSScriptRoot\package.json" "$PSScriptRoot\deploy\package.json" -Force
+Copy-Item "$PSScriptRoot\.npmrc" "$PSScriptRoot\deploy\.npmrc" -Force
